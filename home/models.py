3a4b81f239ddd8dccf1db707ca75133a9903a24f
crowdbotics-apps <team@crowdbotics.com>
2023-03-20 10:52:34 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Creator(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=200,)
+    identity = models.BigIntegerField(max_length=200,)
+    age = models.IntegerField()
